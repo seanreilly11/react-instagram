f994218b78fe4eb34d620d0fd734f02fe5bf8086
Sean <seanreilly123@hotmail.com>
2020-04-20 15:31:15 +1200

    Post icons working

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faHeart as fasHeart,
+    faComment as fasComment,
+    faPaperPlane as fasPlane,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+    faHeart as farHeart,
+    faComment as farComment,
+    faPaperPlane as farPlane,
+} from "@fortawesome/free-regular-svg-icons";
 // import { GlobalContext } from "../context/GlobalState";
-
 export const Posts = ({ post }) => {
-    const { username, image, caption, likes } = post;
+    const { username, image, caption, likes, avatar } = post;
+    const [heart, setHeart] = useState(true);
+    const [comment, setComment] = useState(true);
+    const [plane, setPlane] = useState(true);
     return (
         <div className="post">
-            <h4 className="post-user">{username}</h4>
+            <div className="post-header">
+                <img src={avatar} alt="" />
+                <h4 className="post-user">{username}</h4>
+            </div>
             <div className="post-image">
                 <img src={image} alt="" />
+            </div>
+            <div className="post-icons">
+                <div className="post-icon-heart">
+                    <FontAwesomeIcon
+                        className={
+                            heart === true
+                                ? "post-icon post-icon-heart-reg show-icon"
+                                : "post-icon post-icon-heart-reg hide-icon"
+                        }
+                        icon={farHeart}
+                        onClick={() => setHeart(!heart)}
+                    />
+                    <FontAwesomeIcon
+                        className={
+                            heart === false
+                                ? "post-icon post-icon-heart-solid show-icon"
+                                : "post-icon post-icon-heart-solid hide-icon"
+                        }
+                        icon={fasHeart}
+                        onClick={() => setHeart(!heart)}
+                    />
+                </div>
+                <div className="post-icon-comment">
+                    <FontAwesomeIcon
+                        className={"post-icon post-icon-comment-reg"}
+                        icon={farComment}
+                        onClick={() => setComment(!comment)}
+                    />
+                    <FontAwesomeIcon
+                        className={"post-icon post-icon-comment-solid"}
+                        icon={fasComment}
+                        onClick={() => setComment(!comment)}
+                    />
+                </div>
+                <div className="post-icon-plane">
+                    <FontAwesomeIcon
+                        className={"post-icon post-icon-plane-reg"}
+                        icon={farPlane}
+                        onClick={() => setPlane(!plane)}
+                    />
+                    <FontAwesomeIcon
+                        className={"post-icon post-icon-plane-solid"}
+                        icon={fasPlane}
+                        onClick={() => setPlane(!plane)}
+                    />
+                </div>
             </div>
             <p className="post-likes">{likes} likes</p>
             <p className="post-caption">
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,7 +2,113 @@ import React, { createContext, useReducer, useEffect } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-    posts: [],
+    posts: [
+        {
+            id: 1,
+            username: "cskoyles0",
+            image: "http://dummyimage.com/470x470.jpg/dddddd/000000",
+            caption: "congue diam id ornare imperdiet sapien urna",
+            likes: 81,
+            avatar:
+                "https://robohash.org/aliquamestrerum.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 2,
+            username: "clangcastle1",
+            image: "http://dummyimage.com/470x470.jpg/cc0000/ffffff",
+            caption: "lorem vitae mattis nibh ligula nec sem duis",
+            likes: 63,
+            avatar:
+                "https://robohash.org/estdoloremarchitecto.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 3,
+            username: "eplace2",
+            image: "http://dummyimage.com/470x470.jpg/5fa2dd/ffffff",
+            caption: "tristique est et tempus semper",
+            likes: 3,
+            avatar:
+                "https://robohash.org/maioresexplicaboquia.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 4,
+            username: "jsheeres3",
+            image: "http://dummyimage.com/470x470.jpg/ff4444/ffffff",
+            caption: "turpis sed ante vivamus tortor duis mattis egestas",
+            likes: 10,
+            avatar:
+                "https://robohash.org/magnamsedquos.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 5,
+            username: "bwaldrum4",
+            image: "http://dummyimage.com/470x470.jpg/cc0000/ffffff",
+            caption:
+                "nulla mollis molestie lorem quisque ut erat curabitur gravida nisi at nibh in hac habitasse platea dictumst aliquam",
+            likes: 79,
+            avatar:
+                "https://robohash.org/necessitatibusautcupiditate.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 6,
+            username: "kflisher5",
+            image: "http://dummyimage.com/470x470.jpg/dddddd/000000",
+            caption:
+                "elit proin risus praesent lectus vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum",
+            likes: 63,
+            avatar: "https://robohash.org/eteaaut.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 7,
+            username: "flyptrit6",
+            image: "http://dummyimage.com/470x470.jpg/cc0000/ffffff",
+            caption:
+                "nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam",
+            likes: 60,
+            avatar:
+                "https://robohash.org/sitdoloremenim.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 8,
+            username: "chinkens7",
+            image: "http://dummyimage.com/470x470.jpg/ff4444/ffffff",
+            caption:
+                "venenatis tristique fusce congue diam id ornare imperdiet sapien urna pretium nisl ut volutpat sapien",
+            likes: 93,
+            avatar:
+                "https://robohash.org/perspiciatisvoluptatemlaboriosam.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 9,
+            username: "ncapron8",
+            image: "http://dummyimage.com/470x470.jpg/dddddd/000000",
+            caption:
+                "molestie lorem quisque ut erat curabitur gravida nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae",
+            likes: 49,
+            avatar:
+                "https://robohash.org/nihilnemoquasi.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 10,
+            username: "cworham9",
+            image: "http://dummyimage.com/470x470.jpg/cc0000/ffffff",
+            caption:
+                "ultrices posuere cubilia curae donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin",
+            likes: 43,
+            avatar:
+                "https://robohash.org/distinctiosuntmagni.jpg?size=50x50&set=set1",
+        },
+        {
+            id: 11,
+            username: "mfarrara",
+            image: "http://dummyimage.com/470x470.jpg/ff4444/ffffff",
+            caption:
+                "sapien in sapien iaculis congue vivamus metus arcu adipiscing",
+            likes: 41,
+            avatar:
+                "https://robohash.org/quisadipiscinon.jpg?size=50x50&set=set1",
+        },
+    ],
 };
 
 // creat context
@@ -17,14 +123,15 @@ export const GlobalProvider = ({ children }) => {
             "https://my.api.mockaroo.com/instagram.json?key=c527bcb0"
         );
         const res = await response.json();
+        // console.log(res);
         res.forEach((data) => {
             addPost(data);
         });
     };
 
-    useEffect(() => {
-        getData();
-    });
+    // useEffect(() => {
+    //     getData();
+    // }, []);
 
     // actions
     // function deleteTrans(id) {
